7e57027faea0c1ad9ee451bd4b15295cff3035fc
kshN819 <happy00819@gmail.com>
2024-08-26 16:20:51 +0900

    django day1 MVT Pattern

---
+++ fisa_django/blog/templatetags/custom_filters.py
@@ -0,0 +1,8 @@
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='reverse_string')
+def reverse_string(value):
+    return value[::-1]
---
+++ fisa_django/blog/views.py
@@ -0,0 +1,32 @@
+## blog/views.py
+from django.views.generic import ListView
+from .models import Post
+from django.shortcuts import render
+class PostList(ListView):   # post_list.html, post-list
+    model = Post 
+    # template_name = 'blog/index.html' 
+    # ordering = '-pk' 
+    context_object_name = 'post_list'
+
+
+# Create your views here.
+def index(request):     # 함수를 만들고, 그 함수를 도메인 주소 뒤에 달아서 호출하는 구조
+    posts = Post.objects.all() # 1. 쿼리로 데이터를 모두 가져옵니다
+    # 가져온 데이터는 어디에 뿌려야 하나요? Templates로 보내야겠죠
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts,
+            'my_list': ["apple", "banana", "cherry"], 'my_text': "첫번째 줄 \n 두번째 줄", 'content':'<img src="data/jjangu.jpg/">'
+        }
+    )  # 없는 index.html을 호출하고 있음
+
+
+
+def about_me(request):     # 함수를 만들고, 그 함수를 도메인 주소 뒤에 달아서 호출하는 구조
+    # 가져온 데이터는 어디에 뿌려야 하나요? Templates로 보내야겠죠
+    return render(
+        request,
+        'blog/about_me.html',
+    )
